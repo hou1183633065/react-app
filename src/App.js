bb9e214a07cf04a6a0a0092f765b22bc80c10c74
houzhiqiang <zhq.hou@muheda.com>
2019-01-28 17:40:52 +0800

    webpack @--src

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 // import logo from './logo.svg';
 import './App.css';
 
-import Home from './pages/home'
+import Home from '@/pages/home'
+import About from '@/pages/about'
+import Login from '@/pages/login'
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={import('./pages/about')} />
-          <Route path="/login" component={import('./pages/login')} />
+          <Route path="/about" component={About} />
+          <Route path="/login" component={Login} />
         </Switch>
       </BrowserRouter>
     );
